ed00758d4950745ea4cdeb137a90ba10bb644ddb
suayb <12345678>
2021-10-26 21:14:32 +0300

    init project

--- department-service/src/test/java/io/github/susimsek/demo/services/department/client/EmployeeClientIT.java
+++ department-service/src/test/java/io/github/susimsek/demo/services/department/client/EmployeeClientIT.java
@@ -2,29 +2,22 @@ package io.github.susimsek.demo.services.department.client;
 
 import io.github.susimsek.demo.services.department.DepartmentApplication;
 import io.github.susimsek.demo.services.department.model.Employee;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
-import java.io.IOException;
 import java.util.List;
 
-import static io.github.susimsek.demo.services.department.client.EmployeeClientStub.stubFindEmployeesByDepartmentId;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(classes = DepartmentApplication.class)
-@AutoConfigureWireMock(port = 0)
+@AutoConfigureWireMock(port = 0, stubs = "classpath:/stubs")
 public class EmployeeClientIT {
 
     @Autowired
     EmployeeClient employeeClient;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        stubFindEmployeesByDepartmentId();
-    }
 
     @Test
     public void getAllEmployeesByDepartmentId_whenGetMethod() {
--- department-service/src/test/java/io/github/susimsek/demo/services/department/client/OrganizationClientIT.java
+++ department-service/src/test/java/io/github/susimsek/demo/services/department/client/OrganizationClientIT.java
@@ -2,30 +2,22 @@ package io.github.susimsek.demo.services.department.client;
 
 import io.github.susimsek.demo.services.department.config.OrganizationClientConfig;
 import io.github.susimsek.demo.services.department.model.Organization;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 
-import java.io.IOException;
-
-import static io.github.susimsek.demo.services.department.client.OrganizationClientStub.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = OrganizationClientConfig.class)
-@AutoConfigureWireMock(port = 0)
+@AutoConfigureWireMock(port = 0, stubs = "classpath:/stubs")
 public class OrganizationClientIT {
 
 
     @Autowired
     OrganizationClient organizationClient;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        stubFindOrganizationById();
-    }
 
     @Test
     public void getOrganizationById_whenGetMethod() {
