131ef2be690edbc8a61b4e3ffe800cf119cb1b05
suayb <12345678>
2021-11-15 20:25:21 +0300

    add repository test

--- employee-service/src/main/java/io/github/susimsek/demo/services/employee/repository/EmployeeRepository.java
+++ employee-service/src/main/java/io/github/susimsek/demo/services/employee/repository/EmployeeRepository.java
@@ -2,10 +2,15 @@ package io.github.susimsek.demo.services.employee.repository;
 
 import io.github.susimsek.demo.services.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 	List<Employee> findByDepartmentId(Long departmentId);
+
+	@Query("SELECT e FROM Employee e WHERE e.departmentId = :departmentId")
+	List<Employee> findAllEmployeesByDepartmentId(@Param("departmentId") Long departmentId);
 }
